b204244cc275185bd36e212da57b5d08cea3fca1
romulo <romulo@microdata.com.pe>
2021-12-07 19:18:10 -0500

    cambios en vista

--- 11 Servicios/servicios-http-app/src/app/app.component.ts
+++ 11 Servicios/servicios-http-app/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'servicios-http-app';
+  title = 'Tienda en linea';
+  imagen = '../assets/logo.png'
 }
